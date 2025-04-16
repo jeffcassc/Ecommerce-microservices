ebca68db1e55fdd816c5e35d43521209843c62cd
Yefrey cassiani <yefreycassiani16@gmail.com>
2025-04-15 19:42:07 -0500

    Revert "update cambio de fast api a flask"
    This reverts commit d1db6acf6bf5d31afe38a7e4f190948ab6613123.

--- services/user-service/app/config/settings.py
+++ services/user-service/app/config/settings.py
@@ -3,15 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Config:
-    # Kafka
+class Settings:
     KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
-    USER_REGISTRATION_TOPIC = os.getenv("USER_REGISTRATION_TOPIC", "user-registration")
-    
-    # MongoDB
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongodb:27017/")
     MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "ecommerce_events")
-    MONGO_USER_COLLECTION = os.getenv("MONGO_USER_COLLECTION", "users")
+    USER_SERVICE_PORT = int(os.getenv("USER_SERVICE_PORT", 8000))
     
-    # Application
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+    # Configuración de Kafka Topics
+    USER_REGISTRATION_TOPIC = "user-registration"
+    WELCOME_FLOW_TOPIC = "welcome-flow"
+    
+    # Configuración de logs
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+settings = Settings()
